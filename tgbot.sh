3b1b5c24b5416a08f1c42ff1f45494b254e474db
noobyysauraj <gitsauraj@gmail.com>
2022-09-03 21:04:31 +0530

    Add .info
    Used for getting information about the chat and users.

--- tgbot.sh
+++ tgbot.sh
@@ -47,6 +47,37 @@ iq() {
 		tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "@${USERNAME} calculated IQ Score is ${iq}."
 	fi
 }
+info() {
+        if [ ${RET_REPLIED_MSGGER_ID} != "null" ]; then
+			USERNAME=$(echo $USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			FIRST_NAME=$( echo $FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			LAST_NAME=$(echo $LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			RET_REPLIED_MSGGER_FIRST_NAME=$(echo $RET_REPLIED_MSGGER_FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			RET_REPLIED_MSGGER_LAST_NAME=$(echo $RET_REPLIED_MSGGER_LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			RET_REPLIED_MSGGER_USERNAME=$(echo $RET_REPLIED_MSGGER_USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+			tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
+Message ID \: \`${RET_MSG_ID}\`
+Chat Type \: \`${RET_CHAT_TYPE}\`
+
+User Name \: \@${USERNAME}
+First Name \: \`${FIRST_NAME}\`
+Last Name \: \`${LAST_NAME}\`
+User ID \: \`${MSGGER}\`
+
+Replied user Username \: \@${RET_REPLIED_MSGGER_USERNAME}
+Replied user First Name \: \`${RET_REPLIED_MSGGER_FIRST_NAME}\`
+Replied user Last Name \: \`${RET_REPLIED_MSGGER_LAST_NAME}\`
+Replied user ID \: \`${RET_REPLIED_MSGGER_ID}\`"
+        else
+			tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
+Chat Type \: \`${RET_CHAT_TYPE}\`
+
+User Name \: \@${USERNAME}
+First Name \: \`${FIRST_NAME}\`
+Last Name \: \`${LAST_NAME}\`
+User ID \: \`${MSGGER}\`"
+        fi
+}
 weath() {
 	TRIMMED=${RET_MSG_TEXT#.weath }
 	char=$(echo ${TRIMMED} | tr -d '\n' | wc -c)
@@ -116,8 +147,9 @@ while true; do
 
 	'/start'*) start ;;
 	'.calc'*) calc ;;
-	'.weath'*) weath ;;
 	'.iq'*) iq ;;
+	'.info'*) info ;;
+	'.weath'*) weath ;;
 	esac
 
 	unset RET_MSG_TEXT RET_REPLIED_MSG_ID
