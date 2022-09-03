2cf52c21da161847d8f0b23b0d315278be6f2f3e
noobyysauraj <gitsauraj@gmail.com>
2022-09-03 17:54:50 +0530

    Add .iq
    Use simple algorithm to calulate iq for fun. Not to be taken seriously.

--- tgbot.sh
+++ tgbot.sh
@@ -10,6 +10,9 @@ echo "STARTING BOT"
 
 
 #Defining Function
+start() {
+    tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "A Simple bot from @Ksauraj and @Hakimi0804\."
+}
 round() {
 	# $1 = Your number
 	# $2 = Amount of decimal places
@@ -27,67 +30,80 @@ calc() {
         tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Bruh, did you just entered nonsense, cuz bc ain't happy"
     fi
 }
-start() {
-    tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "A Simple bot from @Ksauraj and @Hakimi0804\."
+iq() {
+	if [[ $USERNAME == 'Ksauraj' ]]; then
+		iq=$(shuf -i 0-8 -n1)
+		iq=$(expr 180 - $iq)
+		tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Calculating your IQ, wait plox..."
+		tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "@${USERNAME} calculated IQ Score is ${iq}."
+	else
+		tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Calculating your IQ, wait plox..."
+		sha=$(echo ${FIRST_NAME} | sha1sum | grep -Eo "[[:digit:]]{2}" | head -n1)
+		md5=$(echo ${USERNAME} | md5sum | grep -Eo "[[:digit:]]{2}" | head -n1)
+		num1=$(expr ${sha} + ${md5} | head -n1)
+		num2=$(shuf -i 0-5 -n1)
+		f=$(expr ${num1} - ${num2} )
+		iq=$(expr 180 - $f )
+		tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "@${USERNAME} calculated IQ Score is ${iq}."
+	fi
 }
 weath() {
 	TRIMMED=${RET_MSG_TEXT#.weath }
 	char=$(echo ${TRIMMED} | tr -d '\n' | wc -c)
 	if [ "$char" == 0 ]; then
-	tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Please provide argument bisi"
+		tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Please provide argument bisi"
 	else
-	location=${TRIMMED}
-	RESULT=$(curl --request GET \
-		--url "https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=c" \
-		--header 'x-rapidapi-host: yahoo-weather5.p.rapidapi.com' \
-		--header "x-rapidapi-key: ${WEATH_API_KEY}")
+		location=${TRIMMED}
+		RESULT=$(curl --request GET \
+			--url "https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=c" \
+			--header 'x-rapidapi-host: yahoo-weather5.p.rapidapi.com' \
+			--header "x-rapidapi-key: ${WEATH_API_KEY}")
 
-	city=$(echo "$RESULT" | jq '.location | .city')
-	woeid=$(echo "$RESULT" | jq '.location | .woeid')
-	country=$(echo "$RESULT" | jq '.location | .country')
-	region=$(echo "$RESULT" | jq '.location | .region')
-	timezone_id=$(echo "$RESULT" | jq '.location | .timezone_id')
-	speed=$(echo "$RESULT" | jq '.current_observation | .wind | .speed')
-	humidity=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .humidity')
-	visibility=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .visibility')
-	pressure=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .pressure')
-	sunrise=$(echo "$RESULT" | jq '.current_observation | .astronomy | .sunrise')
-	sunset=$(echo "$RESULT" | jq '.current_observation | .astronomy | .sunset')
-	low_1=$(echo "$RESULT" | jq '.forecasts[].low' | sed '1!d')
-	low_2=$(echo "$RESULT" | jq '.forecasts[].low' | sed '2!d')
-	low_3=$(echo "$RESULT" | jq '.forecasts[].low' | sed '3!d')
-	high_1=$(echo "$RESULT" | jq '.forecasts[].high' | sed '1!d')
-	high_2=$(echo "$RESULT" | jq '.forecasts[].high' | sed '2!d')
-	high_3=$(echo "$RESULT" | jq '.forecasts[].high' | sed '3!d')
-	day_1=$(echo "$RESULT" | jq '.forecasts[].day' | sed '1!d')
-	day_2=$(echo "$RESULT" | jq '.forecasts[].day' | sed '2!d')
-	day_3=$(echo "$RESULT" | jq '.forecasts[].day' | sed '3!d')
-	weather_1=$(echo "$RESULT" | jq '.forecasts[].text' | sed '1!d')
-	echo "Showing Results for" ${city}"," ${region}"," ${country} > weath.txt
-	echo "Timezone : ${timezone_id}" >> weath.txt
-	echo "Wind Speed : ${speed}km/h" >> weath.txt
-	echo "Humididty : ${humidity}%" >> weath.txt
-	echo "Visibility : ${visibility}%" >> weath.txt
-	echo "Pressure : ${pressure} Hg" >> weath.txt
-	echo "Day : ${day_1}" >> weath.txt
-	echo "Min Temperature : ${low_1}°C" >> weath.txt
-	echo "Min Temperature : ${high_1}°C" >> weath.txt
-	echo "Weather : ${weather_1}" >> weath.txt
-	echo "Sunrise : ${sunrise}" >> weath.txt
-	echo "Sunset : ${sunset}" >> weath.txt
-	echo "" >> weath.txt
-	echo "Forecasts for next 2 days" >> weath.txt
-	echo "" >> weath.txt
-	echo "            Min Temp.      Max Temp." >> weath.txt
-	echo "${day_2}       ${low_2}°C      ${high_2}°C" >> weath.txt
-	echo "${day_3}       ${low_3}°C      ${high_3}°C" >> weath.txt
-	sed -i s/\"//g weath.txt
-	text=$(cat weath.txt)
-	tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "${text}"
-	rm weath.txt
+		city=$(echo "$RESULT" | jq '.location | .city')
+		woeid=$(echo "$RESULT" | jq '.location | .woeid')
+		country=$(echo "$RESULT" | jq '.location | .country')
+		region=$(echo "$RESULT" | jq '.location | .region')
+		timezone_id=$(echo "$RESULT" | jq '.location | .timezone_id')
+		speed=$(echo "$RESULT" | jq '.current_observation | .wind | .speed')
+		humidity=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .humidity')
+		visibility=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .visibility')
+		pressure=$(echo "$RESULT" | jq '.current_observation | .atmosphere | .pressure')
+		sunrise=$(echo "$RESULT" | jq '.current_observation | .astronomy | .sunrise')
+		sunset=$(echo "$RESULT" | jq '.current_observation | .astronomy | .sunset')
+		low_1=$(echo "$RESULT" | jq '.forecasts[].low' | sed '1!d')
+		low_2=$(echo "$RESULT" | jq '.forecasts[].low' | sed '2!d')
+		low_3=$(echo "$RESULT" | jq '.forecasts[].low' | sed '3!d')
+		high_1=$(echo "$RESULT" | jq '.forecasts[].high' | sed '1!d')
+		high_2=$(echo "$RESULT" | jq '.forecasts[].high' | sed '2!d')
+		high_3=$(echo "$RESULT" | jq '.forecasts[].high' | sed '3!d')
+		day_1=$(echo "$RESULT" | jq '.forecasts[].day' | sed '1!d')
+		day_2=$(echo "$RESULT" | jq '.forecasts[].day' | sed '2!d')
+		day_3=$(echo "$RESULT" | jq '.forecasts[].day' | sed '3!d')
+		weather_1=$(echo "$RESULT" | jq '.forecasts[].text' | sed '1!d')
+		echo "Showing Results for" ${city}"," ${region}"," ${country} > weath.txt
+		echo "Timezone : ${timezone_id}" >> weath.txt
+		echo "Wind Speed : ${speed}km/h" >> weath.txt
+		echo "Humididty : ${humidity}%" >> weath.txt
+		echo "Visibility : ${visibility}%" >> weath.txt
+		echo "Pressure : ${pressure} Hg" >> weath.txt
+		echo "Day : ${day_1}" >> weath.txt
+		echo "Min Temperature : ${low_1}°C" >> weath.txt
+		echo "Min Temperature : ${high_1}°C" >> weath.txt
+		echo "Weather : ${weather_1}" >> weath.txt
+		echo "Sunrise : ${sunrise}" >> weath.txt
+		echo "Sunset : ${sunset}" >> weath.txt
+		echo "" >> weath.txt
+		echo "Forecasts for next 2 days" >> weath.txt
+		echo "" >> weath.txt
+		echo "            Min Temp.      Max Temp." >> weath.txt
+		echo "${day_2}       ${low_2}°C      ${high_2}°C" >> weath.txt
+		echo "${day_3}       ${low_3}°C      ${high_3}°C" >> weath.txt
+		sed -i s/\"//g weath.txt
+		text=$(cat weath.txt)
+		tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "${text}"
+		rm weath.txt
 	fi
 }
-
 
 ## While loop
 while true; do
@@ -101,6 +117,7 @@ while true; do
 	'/start'*) start ;;
 	'.calc'*) calc ;;
 	'.weath'*) weath ;;
+	'.iq'*) iq ;;
 	esac
 
 	unset RET_MSG_TEXT RET_REPLIED_MSG_ID
