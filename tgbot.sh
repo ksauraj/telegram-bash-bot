b742c4db264292536e069aaab9e437f1fedec10c
noobyysauraj <gitsauraj@gmail.com>
2022-09-03 21:12:29 +0530

    Add .neofetch
    Use the popular tool for displaying information about the host.

--- tgbot.sh
+++ tgbot.sh
@@ -48,14 +48,14 @@ iq() {
 	fi
 }
 info() {
-        if [ ${RET_REPLIED_MSGGER_ID} != "null" ]; then
-			USERNAME=$(echo $USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			FIRST_NAME=$( echo $FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			LAST_NAME=$(echo $LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			RET_REPLIED_MSGGER_FIRST_NAME=$(echo $RET_REPLIED_MSGGER_FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			RET_REPLIED_MSGGER_LAST_NAME=$(echo $RET_REPLIED_MSGGER_LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			RET_REPLIED_MSGGER_USERNAME=$(echo $RET_REPLIED_MSGGER_USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-			tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
+	if [ ${RET_REPLIED_MSGGER_ID} != "null" ]; then
+		USERNAME=$(echo $USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		FIRST_NAME=$( echo $FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		LAST_NAME=$(echo $LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		RET_REPLIED_MSGGER_FIRST_NAME=$(echo $RET_REPLIED_MSGGER_FIRST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		RET_REPLIED_MSGGER_LAST_NAME=$(echo $RET_REPLIED_MSGGER_LAST_NAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		RET_REPLIED_MSGGER_USERNAME=$(echo $RET_REPLIED_MSGGER_USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+		tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
 Message ID \: \`${RET_MSG_ID}\`
 Chat Type \: \`${RET_CHAT_TYPE}\`
 
@@ -68,15 +68,20 @@ Replied user Username \: \@${RET_REPLIED_MSGGER_USERNAME}
 Replied user First Name \: \`${RET_REPLIED_MSGGER_FIRST_NAME}\`
 Replied user Last Name \: \`${RET_REPLIED_MSGGER_LAST_NAME}\`
 Replied user ID \: \`${RET_REPLIED_MSGGER_ID}\`"
-        else
-			tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
+	else
+		tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Chat ID \: \`${RET_CHAT_ID}\`
 Chat Type \: \`${RET_CHAT_TYPE}\`
 
 User Name \: \@${USERNAME}
 First Name \: \`${FIRST_NAME}\`
 Last Name \: \`${LAST_NAME}\`
 User ID \: \`${MSGGER}\`"
-        fi
+	fi
+}
+neo_fetch() {
+	tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "This may take a while depending upon host..."
+	NEOFETCH_OUTPUT=$(neofetch --stdout)
+	tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "$NEOFETCH_OUTPUT"
 }
 weath() {
 	TRIMMED=${RET_MSG_TEXT#.weath }
@@ -149,6 +154,7 @@ while true; do
 	'.calc'*) calc ;;
 	'.iq'*) iq ;;
 	'.info'*) info ;;
+	'.neofetch'*) neo_fetch ;;
 	'.weath'*) weath ;;
 	esac
 
