8a70e2ba7af3a86ee6118b86df037fe01e235256
noobyysauraj <gitsauraj@gmail.com>
2022-09-03 21:18:33 +0530

    Add .magisk
    Co-authored-by: Hakimi0804 <hakimifirdaus944@gmail.com>

--- tgbot.sh
+++ tgbot.sh
@@ -47,6 +47,21 @@ iq() {
 		tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "@${USERNAME} calculated IQ Score is ${iq}."
 	fi
 }
+magisk() {
+		tg --sendmsg "$RET_CHAT_ID" "Fetching latest Magisk stable"
+		LATEST_STABLE=$(
+			curl -s https://api.github.com/repos/topjohnwu/Magisk/releases/latest |
+				grep "Magisk-v**.*.apk" |
+				cut -d : -f 2,3 |
+				tr -d \" |
+				cut -d, -f2 |
+				tr -d '\n' |
+				tr -d ' '
+		)
+		CANARY="https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk"
+		tg --editmarkdownv2msg "$RET_CHAT_ID" "$SENT_MSG_ID" "[Latest stable]($LATEST_STABLE)
+[Latest canary]($CANARY)"
+}
 info() {
 	if [ ${RET_REPLIED_MSGGER_ID} != "null" ]; then
 		USERNAME=$(echo $USERNAME | sed 's/[`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
@@ -154,6 +169,7 @@ while true; do
 	'.calc'*) calc ;;
 	'.iq'*) iq ;;
 	'.info'*) info ;;
+	'.magisk'*) magisk ;;
 	'.neofetch'*) neo_fetch ;;
 	'.weath'*) weath ;;
 	esac
