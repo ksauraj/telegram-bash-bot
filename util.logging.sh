0926127a41d3b0a141164679c06b23e931d3f4d1
Hakimi0804 <hakimifirdaus944@gmail.com>
2022-09-25 15:20:25 +0800

    logging: Fix indentation

--- util.logging.sh
+++ util.logging.sh
@@ -4,24 +4,24 @@ readonly LOG_FNAME=bot.log
 readonly LOG_DATEFMT="+%d-%m-%y %H:%M:%S.%N"
 
 log() {
-    # Argument to pass:
-    # $1 - log type, ex. -d / --debug
-    # $2 - tag name (script/function name recommended)
-    # $3 - Texts
+	# Argument to pass:
+	# $1 - log type, ex. -d / --debug
+	# $2 - tag name (script/function name recommended)
+	# $3 - Texts
 
-    local arg=$1
-    local logtag=$2
-    local logtype
-    shift 2
+	local arg=$1
+	local logtag=$2
+	local logtype
+	shift 2
 
-    case $arg in
-        -v|--verbose) logtype=V ;;
-        -d|--debug) logtype=D ;;
-        -i|--info) logtype=I ;;
-        -w|--warn) logtype=W ;;
-        -e|--error) logtype=E ;;
-        -f|--fatal) logtype=F ;;
-    esac
+	case $arg in
+		-v|--verbose) logtype=V ;;
+		-d|--debug) logtype=D ;;
+		-i|--info) logtype=I ;;
+		-w|--warn) logtype=W ;;
+		-e|--error) logtype=E ;;
+		-f|--fatal) logtype=F ;;
+	esac
 
-    echo "$logtype: [$(date "$LOG_DATEFMT" | sed 's/......$//')] ($logtag) $*" | tee -a "$LOG_FNAME"
+	echo "$logtype: [$(date "$LOG_DATEFMT" | sed 's/......$//')] ($logtag) $*" | tee -a "$LOG_FNAME"
 }
