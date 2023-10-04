6bae94ce4c089124a85cb628eb2df7cdaa2e19c7
ksauraj <gitsauraj@gmail.com>
2023-10-04 18:38:33 +0530

    bot: add checks for .shuffle

--- bot/shuffle.sh
+++ bot/shuffle.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
 shuffle() {
-    text=$(echo "${RET_REPLIED_MSG_TEXT}" | sed "s/ /\n/g" | shuf | tr '\n' ' ' | sed 's/[][`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
-    tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "${text}"
-}
+    if ( "$RET_REPLIED_MSG_TEXT" == null); 
+    then 
+        tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "Please reply to some message for using shuffle."
+    else
+        text=$(echo "${RET_REPLIED_MSG_TEXT}" | sed "s/ /\n/g" | shuf | tr '\n' ' ' | sed 's/[][`~!@#$%^&*()-_=+{}\|;:",<.>/?'"'"']/\\&/g')
+        tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "${text}"  
+    fi
+  }
